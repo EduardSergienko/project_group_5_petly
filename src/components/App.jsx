480db1d05842953a7b66a633089f8f1267c3671b
EduardSergienko <katleraster@gmail.com>
2022-11-11 12:18:38 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
 
 import SharedLayout from './SharedLayout/SharedLayout';
 import Home from 'pages/Home';
-import LoginPage from 'pages/LoginPage';
 
 // const LoginPage = lazy(() => import('pages/LoginPage'));
 
@@ -12,7 +10,6 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
   );
