e4277b3bdc478396173cb6ba8e5db0696708352f
BalVil <balutavili@gmail.com>
2022-11-11 04:04:34 +0200

    added loginin without styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+
 import SharedLayout from './SharedLayout/SharedLayout';
 import Home from 'pages/Home';
+import LoginPage from 'pages/LoginPage';
+
+// const LoginPage = lazy(() => import('pages/LoginPage'));
+
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
   );
