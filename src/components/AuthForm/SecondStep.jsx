a5b4146049ac5425ce16343b81d9ce9ea63a8474
Dmytro-H13 <garnazhenkobb@gmail.com>
2022-11-14 09:13:27 +0100

    registration

--- src/components/AuthForm/SecondStep.jsx
+++ src/components/AuthForm/SecondStep.jsx
@@ -1,30 +1,41 @@
+import styles from './AuthForm.module.scss';
+
 const SecondStep = ({ formData, setFormData }) => {
   return (
     <>
-      <input
-        type="text"
-        placeholder="Name"
-        value={formData.name}
-        onChange={event =>
-          setFormData({ ...formData, name: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        placeholder="City, region"
-        value={formData.city}
-        onChange={event =>
-          setFormData({ ...formData, city: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        placeholder="Mobile phone"
-        value={formData.phone}
-        onChange={event =>
-          setFormData({ ...formData, phone: event.target.value })
-        }
-      />
+      <div className={styles.formGroup}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Name"
+          value={formData.name}
+          onChange={event =>
+            setFormData({ ...formData, name: event.target.value })
+          }
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="City, region"
+          value={formData.location}
+          onChange={event =>
+            setFormData({ ...formData, location: event.target.value })
+          }
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Mobile phone"
+          value={formData.phone}
+          onChange={event =>
+            setFormData({ ...formData, phone: event.target.value })
+          }
+        />
+      </div>
     </>
   );
 };
--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -4,9 +4,12 @@ const getUserEmail = state => state.auth.user.email;
 
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 
+const getUserToken = state => state.auth.token;
+
 const authSelectors = {
   getUsername,
   getUserEmail,
   getIsLoggedIn,
+  getUserToken,
 };
 export default authSelectors;
