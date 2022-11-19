5b5a95143c0f9ba1f2f771644d2c016d07bc23cd
BalVil <balutavili@gmail.com>
2022-11-19 04:09:37 +0200

    LoginPage, NewsPage styling

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,10 +1,13 @@
 import News from 'components/News/News';
+import Container from 'components/Container/Container';
 
 const NewsPage = () => {
   return (
-    <div>
-      <News title="News" />
-    </div>
+    <main>
+      <Container>
+        <News title="News" />
+      </Container>
+    </main>
   );
 };
 
