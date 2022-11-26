ed69366fbee8766af258c3f7e5cb99acb1d44ba5
EduardSergienko <katleraster@gmail.com>
2022-11-26 11:33:29 +0200

    add pagination and fix news serach

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,121 +1,144 @@
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
-
+import InfiniteScroll from 'react-infinite-scroll-component';
 import styles from 'components/News/News.module.scss';
 import Loader from 'components/Loader';
 import FilterInput from 'helpers/FilterInput';
 import notices from 'helpers/Notification';
 import apiServices from 'services/apiServices';
-import HighlightText from 'helpers/HighlightText';
 
 const News = () => {
-  const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
   const [query, setQuery] = useState('');
-  const [searchParam] = useState(['title', 'description']);
-
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [searchingNewsData, setSearchingNewsData] = useState(null);
+  console.log(items);
+  console.log(searchingNewsData);
+  console.log(query);
   useEffect(() => {
     async function fetchNews() {
       try {
-        const { data } = await apiServices.getNews();
         setIsLoaded(true);
-        setItems(data.data);
+        const { data } = await apiServices.getNews(page);
+
+        setItems(prewState => [...prewState, ...data.data]);
+        if (data.data.length < 9) {
+          setHasMore(false);
+          notices.showSuccess('Thats all News');
+        }
+        setIsLoaded(false);
       } catch (error) {
-        setIsLoaded(true);
-        setError(error);
+        setIsLoaded(false);
+        return notices.showError('Oops, try again');
       }
     }
+
     fetchNews();
-  }, []);
+  }, [page]);
+
+  async function searchNews(e) {
+    e.preventDefault();
+    try {
+      setIsLoaded(true);
+      const { data: searchData } = await apiServices.searchNews(query);
+      setSearchingNewsData(searchData);
+      setIsLoaded(false);
+    } catch (error) {
+      setIsLoaded(false);
+      return notices.showError('Sorry, no news found, try again');
+    }
+  }
 
   const shortenText = (text, max) => {
     return text && text.length > max
       ? `${text.slice(0, max).split(' ').slice(0, -1).join(' ')}...`
       : text;
   };
-
-  const searchNews = items => {
-    const FilteredItems = items.filter(item =>
-      searchParam.some(
-        newItem =>
-          item[newItem].toString().toLowerCase().indexOf(query.toLowerCase()) >
-          -1
-      )
-    );
-    if (FilteredItems < 1) {
-      notices.showWarning('No matching news found');
+  function handleShowMore() {
+    setPage(page + 1);
+  }
+  const handleInputType = e => {
+    setQuery(e.target.value);
+    if (e.target.value.trim() === '') {
+      setSearchingNewsData(null);
+      return;
     }
-    return FilteredItems;
   };
-
-  if (error) {
-    notices.showError(error?.message || 'Oops, something wrong, try again');
-  } else if (!isLoaded) {
-    return <Loader />;
-  } else {
-    return (
-      <div className={styles.newsWrap}>
-        <h1 className={styles.title}>News</h1>
-        <FilterInput value={query} onChange={setQuery} />
-
+  return (
+    <div className={styles.newsWrap}>
+      <h1 className={styles.title}>News</h1>
+      {isLoaded && <Loader />}
+      <FilterInput
+        onSubmit={searchNews}
+        value={query}
+        onChange={handleInputType}
+      />
+      {searchingNewsData ? (
         <ul className={styles.box}>
-          {searchNews(items).map(({ _id, title, description, date, url }) => {
+          {searchingNewsData.map(({ _id, title, description, date, url }) => {
             return (
               <li key={_id} className={styles.newsItem}>
-                {query.length > 1 ? (
-                  <article>
-                    <h2 className={styles.articleName}>
-                      <HighlightText result={title} query={query} />
-                    </h2>
-                    <p className={styles.text}>
-                      <HighlightText result={description} query={query} />
-                    </p>
-
-                    <div className={styles.additional}>
-                      <span className={styles.date}>
-                        {format(new Date(date), 'dd/MM/yyyy')}
-                      </span>
-                      <a
-                        href={url}
-                        className={styles.readMore}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Read more
-                      </a>
-                    </div>
-                  </article>
-                ) : (
-                  <article>
-                    <h2 className={styles.articleName}>
-                      {shortenText(title, 50)}
-                    </h2>
-                    <p className={styles.text}>
-                      {shortenText(description, 225)}
-                    </p>
-                    <div className={styles.additional}>
-                      <span className={styles.date}>
-                        {format(new Date(date), 'dd/MM/yyyy')}
-                      </span>
-                      <a
-                        href={url}
-                        className={styles.readMore}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Read more
-                      </a>
-                    </div>
-                  </article>
-                )}
+                <article>
+                  <h2 className={styles.articleName}>
+                    {shortenText(title, 50)}
+                  </h2>
+                  <p className={styles.text}>{shortenText(description, 225)}</p>
+                  <div className={styles.additional}>
+                    <span className={styles.date}>
+                      {format(new Date(date), 'dd/MM/yyyy')}
+                    </span>
+                    <a
+                      href={url}
+                      className={styles.readMore}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Read more
+                    </a>
+                  </div>
+                </article>
               </li>
             );
           })}
         </ul>
-      </div>
-    );
-  }
+      ) : (
+        <InfiniteScroll
+          className={styles.box}
+          dataLength={items.length}
+          next={handleShowMore}
+          hasMore={hasMore}
+        >
+          {items.map(({ _id, title, description, date, url }) => {
+            return (
+              <li key={_id} className={styles.newsItem}>
+                <article>
+                  <h2 className={styles.articleName}>
+                    {shortenText(title, 50)}
+                  </h2>
+                  <p className={styles.text}>{shortenText(description, 225)}</p>
+                  <div className={styles.additional}>
+                    <span className={styles.date}>
+                      {format(new Date(date), 'dd/MM/yyyy')}
+                    </span>
+                    <a
+                      href={url}
+                      className={styles.readMore}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Read more
+                    </a>
+                  </div>
+                </article>
+              </li>
+            );
+          })}
+        </InfiniteScroll>
+      )}
+    </div>
+  );
 };
 
 export default News;
--- src/helpers/FilterInput/FilterInput.jsx
+++ src/helpers/FilterInput/FilterInput.jsx
@@ -4,25 +4,25 @@ import PropTypes from 'prop-types';
 
 import styles from './FilterInput.module.scss';
 
-const FilterInput = ({ value, onChange }) => {
+const FilterInput = ({ value, onSubmit, onChange }) => {
   return (
-    <div className={styles.search}>
+    <form onSubmit={onSubmit} className={styles.search}>
       <label htmlFor="search" className={styles.label}>
         <DebounceInput
           debounceTimeout={400}
           type="search"
           value={value}
-          onChange={e => onChange(e.target.value.trim())}
+          onChange={onChange}
           name="search"
           id="search"
           placeholder="Search"
           className={styles.input}
         />
-        <i className={styles.searchIcon} aria-hidden="true">
-          <IoMdSearch />
-        </i>
+        <button className={styles.searchBtn} type="submit">
+          <IoMdSearch className={styles.searchIcon} />
+        </button>
       </label>
-    </div>
+    </form>
   );
 };
 
