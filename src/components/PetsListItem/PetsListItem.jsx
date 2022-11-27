a8f245b62d5ba330b643c8b6ff917b87118a1667
Yurii Dvornichenko <raen98@live.ru>
2022-11-27 18:39:04 +0200

    fix

--- src/components/PetsListItem/PetsListItem.jsx
+++ src/components/PetsListItem/PetsListItem.jsx
@@ -1,5 +1,4 @@
 import { useDispatch } from 'react-redux';
-// import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 
 import { userOperations } from '../../redux/user';
@@ -21,7 +20,6 @@ function PetsListItem({
   activeLoader,
   active,
 }) {
-  // const { t } = useTranslation();
   const dispatch = useDispatch();
 
   const deleteButton = id => {
