e2bb2237289e8f6f78dec7dcd3aeea259e061900
EduardSergienko <katleraster@gmail.com>
2022-12-09 15:18:41 +0200

    Update OurFriendsPage.jsx

--- src/pages/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage.jsx
@@ -3,15 +3,23 @@ import apiServices from 'services/apiServices';
 import { useEffect, useState } from 'react';
 import ScrollToTop from 'react-scroll-to-top';
 import ScrollToTopBtn from 'components/ScrollToTopBtn/ScrollToTopBtn';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 const OurFriendsPage = () => {
   const [arr, setArr] = useState([]);
 
   useEffect(() => {
     async function showFriends() {
       try {
+        Loading.arrows({
+          svgColor: '#f59256',
+          backgroundColor: 'rgba(0,0,0,0.1)',
+        });
         const { data } = await apiServices.getAllFriends();
         setArr(data.data);
-      } catch (error) {}
+        Loading.remove();
+      } catch (error) {
+        Loading.remove();
+      }
     }
     showFriends();
   }, []);
