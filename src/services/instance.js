74aab79b324ff7b0200dbde5d06dea15d70acc16
EduardSergienko <katleraster@gmail.com>
2022-12-15 11:31:49 +0200

    Update instance.js

--- src/services/instance.js
+++ src/services/instance.js
@@ -15,7 +15,7 @@ export const injectStore = _store => {
 };
 
 export const axiosInstance = axios.create({
-  baseURL: 'project-group-5-petly-back-end.vercel.app/api',
+  baseURL: 'https://petly-back-end.cyclic.app/api',
   withCredentials: true,
 });
 
@@ -46,7 +46,7 @@ axiosInstance.interceptors.response.use(
     if (
       error.response?.status === 401 &&
       isLoggedIn &&
-      error.request.url !== 'project-group-5-petly-back-end.vercel.app/api'
+      error.request.url !== 'https://petly-back-end.cyclic.app/api'
     ) {
       store.dispatch(authOperations.logOutUser());
     }
